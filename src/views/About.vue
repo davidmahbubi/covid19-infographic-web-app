af1e4725d9d9549b78b0954e207df23fc1b148ec
David Mahbubi <ulrichdavid0370@gmail.com>
2021-02-24 19:08:37 +0700

    Updated dependecies & changed about info

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,11 +4,24 @@
       <h4 class="deep-orange-text">
         Covid 19 Information Web
       </h4>
-      <span>Made with ❤ by <a href="https://mhbproject.com" target="blank">MHB Project</a></span>
+      <span
+        >Made with ❤ by
+        <a href="https://davidmahbubi.my.id" target="blank"
+          >David Mahbubi</a
+        ></span
+      >
+      <br />
+      <span
+        >Brought to you by
+        <a href="https://webmediadigital.com" target="blank">Webmedia</a></span
+      >
       <div class="credit">
         <ul>
           <li>Version : 2.0.0</li>
-          <li>Covid 19 Data API : <a href="https://github.com/mathdroid/covid-19-api">Mathdroid</a></li>
+          <li>
+            Covid 19 Data API :
+            <a href="https://github.com/mathdroid/covid-19-api">Mathdroid</a>
+          </li>
           <li>Data Last Update : {{ getLastUpdate }}</li>
         </ul>
       </div>
@@ -17,21 +30,20 @@
 </template>
 
 <script>
-
-import Axios from 'axios';
+import Axios from "axios";
 
 export default {
   data() {
     return {
-      lastUpdate: ''
-    }
+      lastUpdate: "",
+    };
   },
   created() {
     this.fetchUpdated();
   },
   methods: {
     async fetchUpdated() {
-      const { data } = await Axios.get('https://covid19.mathdro.id/api');
+      const { data } = await Axios.get("https://covid19.mathdro.id/api");
       this.lastUpdate = data.lastUpdate;
     },
   },
@@ -40,11 +52,10 @@ export default {
       return this.lastUpdate;
     },
   },
-}
+};
 </script>
 
 <style scoped>
-
 .about-container {
   text-align: center;
   margin-top: 60px;
